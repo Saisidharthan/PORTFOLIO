9c152007d2957bdfa1df9e91e3824d13e056fb90
Sai Sidharthan <saisidharthan@gamil.com>
2024-07-21 22:03:22 +0530

    footer changed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
             <motion.p whileInView={{opacity:1,x:0}} initial={{opacity:0,x:100}} transition={{duration:0.5}}className="my-4">{CONTACT.phoneNo}</motion.p>
             <a href="#" className="border-b">{CONTACT.email}</a>
         </div>
-        <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <div className="m-8 flex items-center justify-center gap-4 text-4xl">
         <motion.a whileHover={{scale:1.1}} whileTap={{scale:0.9}} href='https://www.linkedin.com/in/saisidharthan/'><FaLinkedin/></motion.a>
 					<motion.a whileHover={{scale:1.1}} whileTap={{scale:0.9}} href='https://github.com/Saisidharthan'><FaGithub/></motion.a>
 					<motion.a whileHover={{scale:1.1}} whileTap={{scale:0.9}} href='https://www.instagram.com/_sai_sidharth_17/'><FaInstagram/></motion.a>
